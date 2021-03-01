58cbbbf27f8d8cc408ffc65b0961b6911e6dbed2
KatyaFAF172 <42991993+KatyaFAF172@users.noreply.github.com>
2021-03-01 12:18:15 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 import numpy as np
 import pandas as pd
+from sklearn.linear_model import LinearRegression
 from sklearn.preprocessing import MinMaxScaler
 from sklearn.model_selection import train_test_split
 from keras.models import Sequential
