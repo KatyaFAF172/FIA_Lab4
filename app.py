a6a3f3c8e2b6baca9bfde8aa980eb94551d325d1
KatyaFAF172 <42991993+KatyaFAF172@users.noreply.github.com>
2021-03-01 12:41:47 +0200

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,6 @@ import pandas as pd
 from sklearn.linear_model import LinearRegression
 from sklearn.preprocessing import MinMaxScaler
 from sklearn.model_selection import train_test_split
-from keras.models import Sequential
 from keras.layers import Dense, Dropout
 from plotly import graph_objs as go
 
