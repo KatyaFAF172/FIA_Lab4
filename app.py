1f7edd3c681f6d54ebc28a393e0aed1f7eb9d932
KatyaFAF172 <42991993+KatyaFAF172@users.noreply.github.com>
2021-03-01 12:15:17 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,9 @@
+import numpy as np
+import pandas as pd
+from sklearn.preprocessing import MinMaxScaler
+from sklearn.model_selection import train_test_split
+from keras.models import Sequential
+from keras.layers import Dense, Dropout
 from plotly import graph_objs as go
 
 app = Flask(__name__)
